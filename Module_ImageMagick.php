e0e949838fd5404f371e40c689adbc1d4e899831
gizmore <gizmore@wechall.net>
2024-01-13 06:38:44 +0100

    'OCR'

--- Module_ImageMagick.php
+++ Module_ImageMagick.php
@@ -1,9 +1,40 @@
 <?php
 namespace GDO\ImageMagick;
 
+use GDO\CLI\Process;
 use GDO\Core\GDO_Module;
+use GDO\Core\GDT_Path;
 
 final class Module_ImageMagick extends GDO_Module
 {
+
+    public function getConfig(): array
+    {
+        return [
+            GDT_Path::make('im_convert_path')->existingFile(),
+            GDT_Path::make('im_probe_path')->existingFile(),
+        ];
+    }
+
+    public function cfgConvertPath(): ?string
+    {
+        return $this->getConfigVar('im_convert_path');
+    }
+
+    public function cfgProbePath(): ?string
+    {
+        return $this->getConfigVar('im_probe_path');
+    }
+
+    public function onInstall(): void
+    {
+        if (!$this->cfgConvertPath())
+        {
+            if ($path = Process::commandPath('magick'))
+            {
+                $this->saveConfigVar('im_convert_path', $path);
+            }
+        }
+    }
 
 }
